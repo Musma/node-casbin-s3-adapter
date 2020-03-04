5bd55615f6a86552ffd1f5ec7fb073deb0500654
Jo SeongHoon <jsh1216@musma.net>
2020-03-04 17:35:02 +0900

    S3Adapter, S3Adapter.test 내용 추가

--- src/S3Adapter.test.ts
+++ src/S3Adapter.test.ts
@@ -1,4 +1,26 @@
+import * as assert from 'assert'
 import * as aws from 'aws-sdk'
+import * as casbin from 'casbin'
+import S3Adapter from '.'
+
+const MODEL = `
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act
+
+[role_definition]
+g = _, _
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = g(r.sub, p.sub) && \
+    (r.obj == p.obj || p.obj == '*') && \
+    (r.act == p.act || p.act == '*')
+`
 
 describe('S3Adapter', () => {
   const S3 = new aws.S3({
@@ -8,10 +30,15 @@ describe('S3Adapter', () => {
 
   describe('Head', () => {
     beforeAll(async done => {
-      await S3.createBucket({
-        Bucket,
-        ObjectLockEnabledForBucket: true
-      }).promise()
+      try {
+        await S3.createBucket({
+          Bucket,
+          ObjectLockEnabledForBucket: true
+        }).promise()
+      } catch (e) {
+        console.log((e as aws.AWSError).message)
+        assert.fail('LocalStack 의심됨')
+      }
       done()
     })
 
@@ -23,13 +50,62 @@ describe('S3Adapter', () => {
         expect(Contents.length).toBeGreaterThanOrEqual(0)
         done()
       })
+
+      describe('초기화 관련', () => {
+        it('맨 처음 S3버킷에 policy 파일이 없어도 초기화되어야 한다. (그냥 빈 파일 취급한다.)', async done => {
+          const model = casbin.newModelFromString(MODEL)
+          const adapter = await S3Adapter.init({
+            S3,
+            Bucket
+          })
+          try {
+            const enforcer = new casbin.Enforcer()
+            await enforcer.initWithModelAndAdapter(model, adapter)
+          } catch (e) {
+            assert.fail(e.message)
+          }
+          done()
+        })
+
+        it('props.forceCreateBucket', () => {
+          assert.ok('TODO')
+        })
+      })
+
+      describe('', () => {
+        it('#savePolicy', async done => {
+          const model = casbin.newModelFromString(MODEL)
+          const adapter = await S3Adapter.init({
+            S3,
+            Bucket
+          })
+          const enforcer = new casbin.Enforcer()
+          await enforcer.initWithModelAndAdapter(model, adapter)
+          await enforcer.addPolicy('p', 'Administrator', '*', 'iam:AddUserToGroup')
+          done()
+        })
+      })
+
+      describe('', () => {
+        it('#removePolicy', async done => {
+          const model = casbin.newModelFromString(MODEL)
+          const adapter = await S3Adapter.init({
+            S3,
+            Bucket
+          })
+          const enforcer = new casbin.Enforcer()
+          await enforcer.initWithModelAndAdapter(model, adapter)
+          await enforcer.removePolicy('p', 'Administrator', '*', 'iam:AddUserToGroup')
+          done()
+        })
+      })
     })
 
-    afterAll(async done => {
-      await S3.deleteBucket({
-        Bucket
-      }).promise()
-      done()
-    })
+    // afterAll(async done => {
+    //   await S3.deleteBucket({
+    //     Bucket
+    //   }).promise()
+    //   done()
+    // })
   })
 })
--- src/S3Adapter.ts
+++ src/S3Adapter.ts
@@ -2,37 +2,151 @@ import * as casbin from 'casbin'
 import * as aws from 'aws-sdk'
 import { boundMethod } from 'autobind-decorator'
 
+export interface S3AdapterProps {
+  /**
+   * S3 버킷 이름
+   */
+  Bucket: aws.S3.BucketName
+
+  /**
+   * @default new aws.S3()
+   */
+  S3?: aws.S3
+
+  /**
+   * 이미 있으면 밀어버리겠다.
+   * @default false
+   */
+  forceCreateBucket?: boolean
+
+  /**
+   * @default DEFAULT_POLICY_FILENAME
+   */
+  policyFileName?: string
+}
+
+const DEFAULT_POLICY_FILENAME = 'policies.csv'
+
 export class S3Adapter implements casbin.Adapter {
-  // eslint-disable-next-line no-useless-constructor
-  constructor (protected readonly s3: aws.S3) {}
+  public static async init (props: S3AdapterProps): Promise<S3Adapter> {
+    const instance = new S3Adapter(props)
+    await instance.init(props)
+    return instance
+  }
+
+  protected readonly S3: aws.S3
+  protected readonly Bucket: aws.S3.BucketName
+  protected readonly Key: aws.S3.ObjectKey
+
+  protected constructor (props: S3AdapterProps) {
+    this.S3 = props.S3 || new aws.S3()
+    this.Bucket = props.Bucket
+    this.Key = props.policyFileName || DEFAULT_POLICY_FILENAME
+  }
 
   @boundMethod
   public async loadPolicy (model: casbin.Model): Promise<void> {
-    casbin.Helper.loadPolicyLine('', model)
+    // TODO: @조성훈 Stream 사용, Line by Line
+    model.clearPolicy()
+
+    try {
+      const { Body = '' } = await this.S3.getObject({ Bucket: this.Bucket, Key: this.Key }).promise()
+      const lines = String(Body).split('\n')
+
+      for (const line of lines) {
+        casbin.Helper.loadPolicyLine(line, model)
+      }
+    } catch (e) {
+      if ((e as aws.AWSError).code === 'NoSuchKey') {
+        // 빈 파일 취급
+      } else {
+        throw e
+      }
+    }
   }
 
   @boundMethod
   public async savePolicy (model: casbin.Model): Promise<boolean> {
-    const astMap = model.model.get('p')
-    if (!astMap) {
+    try {
+      const lines = []
+      for (const key of model.model.keys()) {
+        const astMap = model.model.get(key) || new Map<string, casbin.Assertion>()
+        for (const [ptype, ast] of astMap) {
+          for (const rule of ast.policy) {
+            const line = `${ptype},${rule.join()}`
+            lines.push(line)
+          }
+        }
+      }
+      await this.S3.putObject({
+        Bucket: this.Bucket,
+        Key: this.Key,
+        Body: lines.join('\n')
+      }).promise()
+      return true
+    } catch (e) {
+      if ((e as aws.AWSError)) {
+        console.error(e.message)
+      }
       return false
     }
-
-    return true
   }
 
   @boundMethod
   public async addPolicy (sec: string, ptype: string, rule: string[]): Promise<void> {
-    throw new Error('Method not implemented.')
+    try {
+      console.log(222)
+      const line = `${ptype},${rule.join()}`
+      await this.S3.putObject({
+        Bucket: this.Bucket,
+        Key: this.Key,
+        Body: line
+      }).promise()
+    } catch (e) {
+      if ((e as aws.AWSError)) {
+        console.log(33883)
+        console.error(e.message)
+      }
+    }
   }
 
   @boundMethod
   public async removePolicy (sec: string, ptype: string, rule: string[]): Promise<void> {
-    throw new Error('Method not implemented.')
+    const line = `${ptype},${rule.join()}`
+
+    // throw new Error('Method not implemented.')
   }
 
   @boundMethod
   public async removeFilteredPolicy (sec: string, ptype: string, fieldIndex: number, ...fieldValues: string[]): Promise<void> {
     throw new Error('Method not implemented.')
+  }
+
+  @boundMethod
+  protected async init (props: S3AdapterProps): Promise<void> {
+    if (props.forceCreateBucket) {
+      await this.createOrReplaceBucket(this.Bucket)
+    }
+
+    try {
+      // Bucket 있는지 검사
+      await this.S3.headBucket({ Bucket: this.Bucket }).promise()
+    } catch (e) {
+      if ((e as aws.AWSError)) {
+        console.error(e.message)
+      }
+      throw e
+    }
+  }
+
+  @boundMethod
+  protected async createOrReplaceBucket (Bucket: string): Promise<void> {
+    await this.S3.deleteBucket({
+      Bucket
+    }).promise()
+    await this.S3.createBucket({
+      Bucket,
+      ObjectLockEnabledForBucket: true
+    }).promise()
   }
 }
